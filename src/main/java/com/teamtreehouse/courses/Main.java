e5f9b9de6d2333298de507e365b5d26be0c6e4bd
jzara898 <zara.jerri@gmail.com>
2024-11-26 14:05:33 -0800

    index.hbs

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,12 +1,17 @@
 package com.teamtreehouse.courses;
 
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-        get("/", (req, res) -> "Welcome Students!");
+
+        get("/", (req, res) -> {
+                return new ModelAndView(null, "index.hbs");
+    }, new HandlebarsTemplateEngine());
     }
 }
 /* lambda (functional interface for route object) takes 2 parameters:
@@ -14,3 +19,7 @@ its passed request and response.  And returns Hello World.
  */
 /* (assumed port is 80, or we can specify the port here:
 http://localhost:4567/hello */
+
+/* after adding the 2nd function, re-run the web app but with a slash only
+after the host instead of "hello".  Server is constantly running, waiting for requests.
+ */
