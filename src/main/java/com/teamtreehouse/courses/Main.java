cdd337482fdfb9d804cd5b48cab1d831d4e0cc56
jzara898 <zara.jerri@gmail.com>
2024-11-26 12:49:29 -0800

    Update Main.java

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -6,6 +6,7 @@ import static spark.Spark.get;
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> "Welcome Students!");
     }
 }
 /* lambda (functional interface for route object) takes 2 parameters:
