9c215ae4e0b90b2bb1362a8ad348613ba13b2f51
jzara898 <zara.jerri@gmail.com>
2024-11-26 12:43:24 -0800

    spark setup

---
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -0,0 +1,14 @@
+package com.teamtreehouse.courses;
+
+
+import static spark.Spark.get;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
+/* lambda (functional interface for route object) takes 2 parameters:
+its passed request and response.  And returns Hello World.
+ */
+/* (assumed port is 80 but we can specify the port here: http://localhost:4567/hello */
